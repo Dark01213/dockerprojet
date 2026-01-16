a9e694f6d127703c24751c69a56f8722ad534339
Cedric HU <cedric.hu@efrei.com>
2026-01-16 16:43:45 +0100

    ajout de fichier pour faire le lien entre les 2 bases

--- RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -2,6 +2,11 @@ package com.ingnum.rentalservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class BonjourController {
@@ -9,5 +14,18 @@ public class BonjourController {
     @GetMapping("/bonjour")
     public String bonjour() {
         return "bonjour";
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(BonjourController.class);
+    @Value("${customer.service.url}")
+    private String customerServiceUrl;
+
+    @GetMapping("/customer/{name}")
+    public String bonjourCustomer(@PathVariable String name) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = customerServiceUrl + "/index.php?name=" + name;
+        logger.info("Requesting URL: " + url);
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
     }
 }
